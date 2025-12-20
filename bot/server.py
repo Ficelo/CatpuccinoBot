f05ea0ab4c57ddf393b88119eee218645c7504c0
Ficelo <teiva.trillard@gmail.com>
2025-12-20 22:57:38 +0100

    Fix CORS error ?

--- bot/server.py
+++ bot/server.py
@@ -23,6 +23,8 @@ class MyHandler(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header("Access-Control-Allow-Methods", "GET, OPTIONS")
+            self.send_header("Access-Control-Allow-Headers", "Content-Type")
             self.end_headers()
 
             logs = [
